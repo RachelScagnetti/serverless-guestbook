ada0e3e2c389bc676adc5a1567730af03fd0599d
RachelScagnetti <40798002+RachelScagnetti@users.noreply.github.com>
2022-08-18 07:41:55 -0400

    Update guestbook.js

--- guestbook.js
+++ guestbook.js
@@ -1,7 +1,7 @@
 /**
  * Web application
  */
-const apiUrl = '';
+const apiUrl = 'https://4384d8b9.us-south.apigw.appdomain.cloud/guestbook';
 const guestbook = {
   // retrieve the existing guestbook entries
   get() {
